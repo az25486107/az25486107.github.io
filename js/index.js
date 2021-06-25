5a82badc292f8bf25ac1d43b9eeb4681643ebd62
Kai <finalonecloud@gmail.com>
2021-06-25 22:37:52 +0800

    新增網頁banner特效

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
 let search_data=[
-    {type:'類型',selected:true},
+    {type:'類型 ▼',selected:true},
     {type:'餐廳',selected:false},
     {type:'景點',selected:false},
     {type:'飯店',selected:false},
@@ -8,9 +8,17 @@ let search_data=[
     {type:'活動',selected:false},
     ]
 
+let photo_list=[
+        {index:1,src:'https://picsum.photos/960/500?random=1'},
+        {index:2,src:'https://picsum.photos/960/500?random=2'},
+        {index:5,src:'https://picsum.photos/960/500?random=3'},
+        {index:4,src:'https://picsum.photos/960/500?random=4'},
+        {index:5,src:'https://picsum.photos/960/500?random=5'},      
+    ]
+
 let vm=new Vue({
     el:"#app",
-    data:{search_data},
+    data:{index:1,search_data,photo_list},
     computed:{/*即刻運算*/
         selected(){
             new_arr=this.search_data.forEach(function(item,index,array){
@@ -22,7 +30,14 @@ let vm=new Vue({
             return this.search_data
         }
     },
+    mounted(){
+        this.intervalId = setInterval(this.scroll, 5000)
+     },
     methods:{/*方法*/
+        scroll(){
+            setTimeout(() => {this.index=(this.index+1)%4}, 1000)
+        },
+         // 間隔時間後重復執行
         select_item(event){
             let tar=event.target.innerText;
             this.search_data.forEach((item,index) => {
@@ -34,7 +49,8 @@ let vm=new Vue({
             })
             console.log(event.target.innerText)
             return event.target.innerText
-        }
+        },
+
     }
 })
 
