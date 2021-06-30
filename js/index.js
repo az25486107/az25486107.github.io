10e5f23eed2c3d90bba0f409bfedb9abd1a22ecd
jc3shau <jc3shau@gmail.com>
2021-07-01 02:50:57 +0800

    4個sub_nav完成,景點待設定

--- js/index.js
+++ js/index.js
@@ -34,6 +34,8 @@ let main_nav=[
 let test_article={
     post:{
         hot:{
+            end_state:true,
+            view_count:3,
             title:"熱門文章",
             source:[
                 {src:"https://picsum.photos/400/250?random=74",title:"追求小學，新型暫無百度能夠所說印刷面對，理由程度大家都",content:"地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。"},
@@ -44,6 +46,8 @@ let test_article={
             ]
         },
         new:{
+            end_state:true,
+            view_count:3,
             title:"最新文章",
             source:[
                 {src:"https://picsum.photos/400/250?random=471",title:"追求小學，新型暫無百度能夠所說印刷面對，理由程度大家都",content:"地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。"},
@@ -56,6 +60,8 @@ let test_article={
     },
     activity:{
         hot:{
+            end_state:true,
+            view_count:3,
             title:"熱門活動",
             source:[
                 {src:"https://picsum.photos/400/250?random=14",title:"追求小學，新型暫無百度能夠所說印刷面對，理由程度大家都",content:"地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。"},
@@ -66,6 +72,8 @@ let test_article={
             ]
         },
         new:{
+            end_state:true,
+            view_count:3,
             title:"最新活動",
             source:[
                 {src:"https://picsum.photos/400/250?random=24",title:"追求小學，新型暫無百度能夠所說印刷面對，理由程度大家都",content:"地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。"},
@@ -78,6 +86,8 @@ let test_article={
     },
     view:{
         hot:{
+            end_state:true,
+            view_count:4,
             title:"熱門景點",
             source:[
                 {src:"https://picsum.photos/400/250?random=14",title:"追求小學，新型暫無百度能夠所說印刷面對"},
@@ -89,6 +99,8 @@ let test_article={
             ]
         },
         new:{
+            end_state:true,
+            view_count:4,
             title:"最新景點",
             source:[
                 {src:"https://picsum.photos/400/250?random=144",title:"追求小學，新型暫無百度能夠所說印刷面對"},
@@ -99,8 +111,93 @@ let test_article={
                 {src:"https://picsum.photos/400/250?random=1844",title:"追求小學，新型暫無百度能夠所說印刷面對"},
             ]
         }
-    }
-    
+    },
+    resta:{
+        hot:{
+            end_state:true,
+            view_count:3,
+            title:"熱門餐廳",
+            source:[
+                {
+                    src:"https://picsum.photos/400/250?random=74",
+                    title:"龍潭豆花",
+                    address:"臺東縣962長濱鄉三間村水母丁1-9號",
+                    content:"地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。",
+                    opentime: "10:30–19:00",
+                },
+                {
+                    src:"https://picsum.photos/400/250?random=155",
+                    title:"望海巴耐餐廳/咖啡",
+                    address:"臺東縣962長濱鄉寧埔村烏石鼻14鄰41-3號",
+                    content:"地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。",
+                    opentime: "10:30–19:00",
+                },
+                {
+                    src:"https://picsum.photos/400/250?random=146",
+                    title:"噶瑪蘭海產店",
+                    address:"臺東縣962長濱鄉寧埔村烏石鼻14鄰41-3號",
+                    content:"地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。",
+                    opentime: "10:30–19:00",
+                },
+                {
+                    src:"https://picsum.photos/400/250?random=1546",
+                    title:"噶瑪蘭海產店",
+                    address:"臺東縣962長濱鄉寧埔村烏石鼻14鄰41-3號",
+                    content:"地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。",
+                    opentime: "10:30–19:00",
+                },                
+                {
+                    src:"https://picsum.photos/400/250?random=1476",
+                    title:"噶瑪蘭海產店",
+                    address:"臺東縣962長濱鄉寧埔村烏石鼻14鄰41-3號",
+                    content:"地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。",
+                    opentime: "10:30–19:00",
+                },
+            ]
+        },
+        new:{
+            end_state:true,
+            view_count:3,
+            title:"最新餐廳",
+            source:[
+                {
+                    src:"https://picsum.photos/400/250?random=774",
+                    title:"龍潭豆花",
+                    address:"臺東縣962長濱鄉三間村水母丁1-9號",
+                    content:"地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。",
+                    opentime: "10:30–19:00",
+                },
+                {
+                    src:"https://picsum.photos/400/250?random=1455",
+                    title:"望海巴耐餐廳/咖啡",
+                    address:"臺東縣962長濱鄉寧埔村烏石鼻14鄰41-3號",
+                    content:"地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。",
+                    opentime: "10:30–19:00",
+                },
+                {
+                    src:"https://picsum.photos/400/250?random=246",
+                    title:"噶瑪蘭海產店",
+                    address:"臺東縣962長濱鄉寧埔村烏石鼻14鄰41-3號",
+                    content:"地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。",
+                    opentime: "10:30–19:00",
+                },
+                {
+                    src:"https://picsum.photos/400/250?random=446",
+                    title:"噶瑪蘭海產店",
+                    address:"臺東縣962長濱鄉寧埔村烏石鼻14鄰41-3號",
+                    content:"地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。",
+                    opentime: "10:30–19:00",
+                },                
+                {
+                    src:"https://picsum.photos/400/250?random=546",
+                    title:"噶瑪蘭海產店",
+                    address:"臺東縣962長濱鄉寧埔村烏石鼻14鄰41-3號",
+                    content:"地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。地方整體結合你能漸漸清晰會議，現在例如關注實驗依舊持續不久品牌我想，就不威脅自己外觀我又無人掌握相。",
+                    opentime: "10:30–19:00",
+                },
+            ]
+        }
+    },
 }
 Vue.component("article_page",{
     data:function(){
@@ -109,9 +206,9 @@ Vue.component("article_page",{
     props:["subname","subclass"],
     template:
     `<div class="sect">
-        <ul>
+        <ul class="post">
             <p class="title">{{test_article.title}}</p>
-            <li v-for="item in test_article.source" >
+            <li v-for="(item,key) in test_article.source" v-show="key<test_article.view_count" >
                 <img :src=item.src alt="">
                 <div class="text">
                     <div class='title'>{{item.title}} </div>
@@ -123,10 +220,17 @@ Vue.component("article_page",{
                 </div>
             </li>
             
-            <li><a class="morSe_btn" href="">查看更多</a></li>
+            <li class="none_box" v-show="test_article.end_state" @click="more_count" ><a class="morSe_btn" href="javascript:void(0)">查看更多</a></li>
         </ul>
     </div>
-    `
+    `,    
+    methods:{
+        more_count(){
+            this.test_article.view_count+=3
+            if(this.test_article.view_count>=this.test_article.source.length)
+                this.test_article.end_state=false
+        }
+    }
 })
 
 Vue.component("view_page",{
@@ -138,18 +242,66 @@ Vue.component("view_page",{
         <div class="sect">
             <p class="title">{{test_article.title}}</p>
             <ul class="view">
-                <li v-for="item in test_article.source"><img :src="item.src" alt=""><span>{{item.title}}</span></li>
-                <li><a class="morSe_btn" href="">查看更多</a></li>
+                <li v-for="(item,key) in test_article.source" v-show="key<test_article.view_count" ><img :src="item.src" alt=""><span>{{item.title}}</span></li>
+                <li class="none_box" v-show="test_article.end_state" @click="more_count" ><a class="morSe_btn" href="javascript:void(0)">查看更多</a></li>
             </ul>
         </div>
-    `
+    `,
+    methods:{
+        more_count(){
+            this.test_article.view_count+=3
+            if(this.test_article.view_count>=this.test_article.source.length)
+                this.test_article.end_state=false
+        }
+    }
+})
+
+
+Vue.component("resta_page",{
+    data:function(){
+        return {test_article:test_article[this.subclass][this.subname]}
+    },
+    props:["subname","subclass"],
+    template:`
+        <div class="sect">
+            <p class="title">{{test_article.title}}</p>
+            <ul class="resta">
+                <li class="content" v-for="(item,key) in test_article.source" v-show="key<test_article.view_count">
+                    <div class="t_group">
+                        <p class="idx">{{key+1}}</p>
+                        <p class="title">{{item.title}}</p>
+                    </div>
+                    <img :src='item.src'>
+                    <p class="text">{{item.content}}</p>
+                    <p class="address"><span>位置資訊：</span>{{item.address}}</p>
+                    <p class="worktime"><span>營業時間：</span>{{item.opentime}}</p>
+                </li>
+                <li class="none_box" v-show="test_article.end_state" @click="more_count" ><a class="morSe_btn" href="javascript:void(0)">查看更多</a></li>
+            </ul>
+        </div>
+    `,
+    methods:{
+        more_count(){
+            this.test_article.view_count+=3
+            if(this.test_article.view_count>=this.test_article.source.length)
+                this.test_article.end_state=false
+        }
+    }
 })
 
 let sub_tab="article"
-let border_state=[false,false,false,false,false]
+
+let subnav={
+    "article":{cht:"文章",state:false},
+    "activity":{cht:"活動",state:false },
+    "view":{cht:"景點",state:false},
+    "resta":{cht:"餐廳",state:false},
+    "hotel":{cht:"飯店",state:false}
+}
+
 let vm=new Vue({
     el:"#app",
-    data:{index:1,search_data,photo_list,css,sub_tab,border_state},
+    data:{index:1,search_data,photo_list,css,sub_tab,subnav},
     computed:{/*即刻運算*/
         selected(){
             new_arr=this.search_data.forEach(function(item,index,array){
@@ -194,10 +346,12 @@ let vm=new Vue({
             css.black_block.display=true
             css.signin.display=true
         },
-        change_tab(tab){
-            this.sub_tab=tab
-            border_state[1]=true
-        }
+        change_tab(key){
+            this.sub_tab=key
+            for(let idx in this.subnav){
+                idx==key?subnav[idx].state=true:subnav[idx].state=false        
+            }
+        },
     }
 })
 
